74f96c01bf818a5919db306251a69246816871d2
Mustafa YILMAN <91565862+WuSeLeWu@users.noreply.github.com>
2024-08-29 10:53:05 +0300

    404 added

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import App from "./App";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename={process.env.PUBLIC_URL || ""}>
+    <BrowserRouter basename="/Discord-Web-Clone">
       <App />
     </BrowserRouter>
   </React.StrictMode>
